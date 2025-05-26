3c1d8fbef98218e38afd9be02c74112339542ecf
Bautista Aris <lennyaris10@gmail.com>
2025-05-26 19:21:43 -0300

    colores y cursor blog

--- Blog/script.js
+++ Blog/script.js
@@ -49,4 +49,10 @@ document.addEventListener("DOMContentLoaded", () => {
             artcaractUl.classList.toggle('active');
         });
     }
+
+    const cursor = document.getElementById('cursor');
+    document.addEventListener('mousemove', e => {
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+    });
 });
